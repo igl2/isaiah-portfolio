a3a4e9daababcbafab68121ed82ca0f76ac85564
Isaiah Lleva <isaiahlleva@gmail.com>
2022-07-07 12:21:07 -0500

    added contact, loader animation, and email form

---
+++ src/components/About/index.js
@@ -0,0 +1,57 @@
+import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
+import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngular, faCss3, faGit, faHtml5, faJs, faReact } from '@fortawesome/free-brands-svg-icons';
+import Loader from 'react-loaders';
+
+const About = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+        setTimeout(() => {
+          return setLetterClass('text-animate-hover')
+        }, 4000)
+      }, [])
+
+    return (
+        <>
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>
+                    <AnimatedLetters
+                    letterClass={letterClass} 
+                    strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+                    idx={15} />
+                </h1>
+                <p></p>
+            </div>
+            <div className='stage-cube-cont'>
+                <div className='cubespinner'>
+                    <div className='face1'>
+                        <FontAwesomeIcon icon={faHtml5} color="#DD0031" />
+                    </div>
+                    <div className='face2'>
+                        <FontAwesomeIcon icon={faCss3} color="#F06529" />
+                    </div>
+                    <div className='face3'>
+                        <FontAwesomeIcon icon={faJs} color="#28A4D9" />
+                    </div>
+                    <div className='face4'>
+                        <FontAwesomeIcon icon={faAngular} color="#5ED4f4" />
+                    </div>
+                    <div className='face5'>
+                        <FontAwesomeIcon icon={faReact} color="#EFD81D" />
+                    </div>
+                    <div className='face6'>
+                        <FontAwesomeIcon icon={faGit} color="#EC4D28" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <Loader type='pacman' />
+        </>
+    )
+}
+
+export default About
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,11 +3,13 @@ import LogoTitle from '../../assets/images/logo-s.png';
 import { Link } from 'react-router-dom';
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
+import Logo from "./Logo";
+import Loader from "react-loaders";
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
-    const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n'];
-    const jobArray = ['w', 'e', 'b',' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.'];
+    const nameArray = ['I', 's', 'a', 'i', 'a', 'h', '.'];
+    const jobArray = ['S', 'o', 'f','t','w','a','r','e',' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.'];
  
     useEffect(() => {
         setTimeout(() => {
@@ -16,6 +18,7 @@ const Home = () => {
       }, [])
 
     return (
+        <>
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
@@ -24,15 +27,19 @@ const Home = () => {
                     <br/>
                     <span className={`${letterClass} _13`}>I</span>
                     <span className={`${letterClass} _14`}>'m</span>
+                    <span> </span>
                     <img src={LogoTitle} alt="developer"/>
                     <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15}/>
                     <br />
                     <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22}/>
                 </h1>
-                <h2>Frontend Developer / JavaScript Expert</h2>
+                <h2>Frontend Web Developer / Aspiring Software Engineer</h2>
                 <Link to="/contact" className="flat-button">CONTACT ME</Link>
             </div>
+            <Logo />
         </div>
+        <Loader type='pacman' />
+        </>
     );
 }
 
